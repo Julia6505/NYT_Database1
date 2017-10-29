6f33207a0df56065bd8e4f19b4aa69875db97a23
Julia McGee <juliamcgee@Julias-MacBook-Pro.local>
2017-10-29 10:37:53 -0400

    made new repo for local computer

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Books from "./pages/Articles";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <div>
+    <Nav />
+    <Articles />
+  </div>
+);
+
+export default App;
---
+++ client/src/components/Saved/index.js
@@ -0,0 +1,3 @@
+export * from "./Saved";
+export * from "./DeleteBtn";
+export * from "./SavedItem";
